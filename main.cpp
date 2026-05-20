8ac3c035d9f5f9fc72a01c588fc19f41689e87d8
Gabriel Mercado <mercado.gabriel91@hotmail.com>
2026-05-19 21:55:41 -0300

    init commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <filesystem>
+#include <poppler-document.h>
+
+namespace fs = std::filesystem;
+
+// This check_dependencies function should be removed after development since it won't be necessary onced compiled
+int check_dependencies() {
+    try {
+        poppler::document *document = poppler::document::load_from_file("/Gabriel-Mercado-CV.pdf");
+        std::cout << "Poppler-cpp header found!" << std::endl;
+        return 0;
+    } catch (const char *msg) {
+        // Code to handle the error
+        throw std::runtime_error(msg);
+    }
+}
+
+int main() {
+    check_dependencies();
+
+    return 0;
+}
